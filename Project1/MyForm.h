d9980181003745ca1fa02e402946215bdc7b2a2e
Railag009 <Solovevkirill26072002@ya.ru>
2023-03-26 16:59:17 +0300

    Добавьте файлы проекта.

---
+++ Project1/MyForm.h
@@ -0,0 +1,197 @@
+#pragma once
+#include <iostream>
+#include <math.h>
+
+namespace Project1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::DataVisualization::Charting::Chart^ chart1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::Windows::Forms::DataVisualization::Charting::ChartArea^ chartArea1 = (gcnew System::Windows::Forms::DataVisualization::Charting::ChartArea());
+			System::Windows::Forms::DataVisualization::Charting::Legend^ legend1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Legend());
+			System::Windows::Forms::DataVisualization::Charting::Series^ series1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Series());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->chart1 = (gcnew System::Windows::Forms::DataVisualization::Charting::Chart());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(393, 517);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(101, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Нижняя граница Х";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(392, 542);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(103, 13);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Верхняя граница Х";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(392, 570);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(168, 13);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"Значение разбиение интервала";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(500, 514);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(500, 539);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->TabIndex = 4;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(396, 586);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 20);
+			this->textBox3->TabIndex = 5;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(702, 534);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(147, 61);
+			this->button1->TabIndex = 6;
+			this->button1->Text = L"График";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// chart1
+			// 
+			chartArea1->Name = L"ChartArea1";
+			this->chart1->ChartAreas->Add(chartArea1);
+			legend1->Name = L"Legend1";
+			this->chart1->Legends->Add(legend1);
+			this->chart1->Location = System::Drawing::Point(53, 23);
+			this->chart1->Name = L"chart1";
+			series1->ChartArea = L"ChartArea1";
+			series1->ChartType = System::Windows::Forms::DataVisualization::Charting::SeriesChartType::Spline;
+			series1->Legend = L"Legend1";
+			series1->Name = L"Series1";
+			this->chart1->Series->Add(series1);
+			this->chart1->Size = System::Drawing::Size(1231, 445);
+			this->chart1->TabIndex = 7;
+			this->chart1->Text = L"chart1";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1377, 648);
+			this->Controls->Add(this->chart1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->chart1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+		int a, b;
+
+		chart1->Series["Series1"]->Points->Clear();
+		float c = Convert::ToDouble(textBox1->Text);
+		float d = Convert::ToDouble(textBox2->Text);
+		float N = Convert::ToDouble(textBox3->Text);
+
+		a = 0;
+		b = 1;
+
+		float h = ((b - a) / N);
+		for (float i = c; i <= d; i += h)
+		{
+			
+			float y = ((-pow(b, 2) * cos(b + i)) + 2 * (b * sin((b + i)) + cos(b + i))) - (-pow(a, 2) * cos((a + i)) + 2 * (a * sin((a + i)) + cos(a + i)));
+			
+			chart1->Series["Series1"]->Points->AddXY(i, y);
+
+		}
+
+
+	}
+};
+}
